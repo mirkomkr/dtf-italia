d64f24acd49322cc3e2c866f6b1cb840ab660c05
mirkomrk <bionikle746@gmail.com>
2026-01-17 19:13:44 +0100

    import { SpeedInsights } from "@vercel/speed-insights/next"

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import "./globals.css";
 import Header from "@/components/Header";
 import dynamic from 'next/dynamic';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const Footer = dynamic(() => import('@/components/Footer'), { 
@@ -58,6 +59,7 @@ export default function RootLayout({ children }) {
 
       <body className={`${inter.className} bg-gray-50 text-gray-900 antialiased min-h-screen flex flex-col`}>
         <Header />
+        <SpeedInsights />
         <main id="main-content" className="relative flex-1 flex flex-col">
           {children}
         </main>
